0d119635e10ce7879fa3c889946455812e6edb6e
LubnaAjanal <lubnaajanal@gmail.com>
2024-06-29 15:38:20 +0530

    Lubna: correcting the query to show attendance

--- app/Http/Controllers/AttendController.php
+++ app/Http/Controllers/AttendController.php
@@ -25,16 +25,40 @@ class AttendController extends Controller
         $std = $request->session()->get('std');
         $dv = $request->session()->get('dv');
         $student_id = $request->session()->get('student_id');
+        $branch_id = $request->session()->get('branch_id');
+        $from_date = $request->input('fdate');
+        $to_date = $request->input('tdate');
 
         // Query the database
-        $attendance = DB::table('ark_attend')
-            ->where('std', $std)
-            ->where('dv', $dv)
-            ->where('student_id', $student_id)
-            ->whereBetween('date', [$request->fdate, $request->tdate])
-            ->get();
+        // $attendance = DB::table('ark_atten')
+        //     ->where('std', $std)
+        //     ->where('dv', $dv)
+        //     ->where('student_id', $student_id)
+        //     ->whereBetween('date', [$request->fdate, $request->tdate])
+        //     ->get();
+        $query = DB::select("
+                SELECT date,atn,id 
+                FROM ark_atten 
+                WHERE std = ? AND dv = ? AND student_id = ? AND date BETWEEN ? AND ? ORDER BY date",
+                [$std, $dv, $student_id, $from_date, $to_date]);
+
+        $query1 = DB::select("
+                SELECT odate,day,id 
+                FROM ark_dayoff 
+                WHERE  branch_id = ? AND odate BETWEEN ? AND ? ORDER BY odate",
+                [$branch_id, $from_date, $to_date]);
+
+            $attendance = $query;
+            $day_off = $query1;
+            $combinedRecords = array_merge($attendance, $day_off);
+            // return $query1;
+            // return $combinedRecords;
+
+            
 
         // Return the data to the attendTable view
-        return view('attendTable', compact('attendance','request'));
+        return view('attendTable', compact('combinedRecords', 'from_date', 'to_date'));
+        // return view('attendTable', ['attendance' => $query,'day_off' => $query1]);
+        // return view('attendTable', compact('attendance','request'));
     }
 }
