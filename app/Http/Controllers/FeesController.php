3618472150026a31b53bb517376c297e793c5511
gazala1628 <cm23jm1304@gmail.com>
2024-07-02 11:23:19 +0530

    gazala: corrected feehistory query

--- app/Http/Controllers/FeesController.php
+++ app/Http/Controllers/FeesController.php
@@ -20,7 +20,7 @@ class FeesController extends Controller
             ->where('student_id', $stud_id)
             ->pluck('category');
 
-            $query = "SELECT
+        $query = "SELECT
             a.bhead_id,
             b.head,
             b.amount AS total_amount,
@@ -62,18 +62,18 @@ class FeesController extends Controller
         b.amount AS total_amount,
         COALESCE(SUM(a.amount), 0) AS paid_amount,
         b.amount - COALESCE(SUM(a.amount), 0) AS balance_amount
-        FROM
-            sal_fees_bhead b
-        LEFT JOIN
-            sal_fees_trans a ON b.head_id = a.bhead_id
-        WHERE
-            a.student_id = '$stud_id'
+    FROM
+        sal_fees_bhead b
+    LEFT JOIN
+        sal_fees_trans a ON b.head_id = a.bhead_id
+    WHERE
+    a.student_id = '$stud_id'
             AND b.branch_id = '$branch_id'
             AND b.std = '$std'
             AND b.academic_year = '$academic_year'
-        GROUP BY
-            b.head";
-
+    GROUP BY
+        b.head, b.amount;
+    ";
         $feeBal = DB::select($query);
 
         return view('fees.balanceFees', compact('academic_year', 'feeBal', 'category'));
