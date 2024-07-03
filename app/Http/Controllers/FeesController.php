8381296c843ae39ebecc25c677bf3acc04fe84c1
gazala1628 <cm23jm1304@gmail.com>
2024-07-03 15:22:03 +0530

    gazala: corrected balance fee code

--- app/Http/Controllers/FeesController.php
+++ app/Http/Controllers/FeesController.php
@@ -18,14 +18,14 @@ class FeesController extends Controller
             ->select('category')
             ->distinct()
             ->where('student_id', $stud_id)
-            ->pluck('category');
+            ->pluck('category')->toArray(); 
 
         $query = "SELECT
             a.bhead_id,
             b.head,
+            a.month,
             b.amount AS total_amount,
             a.amount AS paid_amount,
-            b.amount - COALESCE((SELECT SUM(at.amount) FROM sal_fees_trans at WHERE b.head_id = at.bhead_id AND at.student_id = '$stud_id' AND at.academic_year = '$academic_year'), 0) AS balance_amount,
             a.pdate
         FROM
             sal_fees_trans a
@@ -55,25 +55,40 @@ class FeesController extends Controller
             ->select('category')
             ->distinct()
             ->where('student_id', $stud_id)
-            ->pluck('category');
+            ->pluck('category')->toArray(); 
 
+        $cat_id = DB::table('sal_fees_trans')
+            ->select('category_id')
+            ->distinct()
+            ->where('student_id', $stud_id)
+            ->pluck('category_id')->toArray(); 
+
+            // dd($cat_id);
+    
+        // Constructing the SQL query
         $query = "SELECT
-        b.head,
-        b.amount AS total_amount,
-        COALESCE(SUM(a.amount), 0) AS paid_amount,
-        b.amount - COALESCE(SUM(a.amount), 0) AS balance_amount
-    FROM
-        sal_fees_bhead b
-    LEFT JOIN
-        sal_fees_trans a ON b.head_id = a.bhead_id
-    WHERE
-    a.student_id = '$stud_id'
-            AND b.branch_id = '$branch_id'
+            b.head,
+            CASE WHEN b.head_id = 16 THEN (b.amount * 12) ELSE b.amount END AS total_amount,
+            a.amount AS paid_amount
+        FROM
+            sal_fees_bhead b
+        LEFT JOIN (
+            SELECT
+                bhead_id,
+                SUM(amount) AS amount
+            FROM
+                sal_fees_trans
+            WHERE
+                student_id = '$stud_id'
+            GROUP BY
+                bhead_id
+        ) a ON b.head_id = a.bhead_id
+        WHERE
+            b.branch_id = '$branch_id'
             AND b.std = '$std'
             AND b.academic_year = '$academic_year'
-    GROUP BY
-        b.head, b.amount;
-    ";
+            AND b.category_id IN (" . implode(',', $cat_id) . ")";
+
         $feeBal = DB::select($query);
 
         return view('fees.balanceFees', compact('academic_year', 'feeBal', 'category'));
