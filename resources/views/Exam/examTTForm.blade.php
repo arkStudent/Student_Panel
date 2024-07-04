56885d5a469332759347d83a00f435b8b2357508
gazala1628 <cm23jm1304@gmail.com>
2024-07-04 14:51:22 +0530

    gazala: corrected from and to date

--- resources/views/Exam/examTTForm.blade.php
+++ resources/views/Exam/examTTForm.blade.php
@@ -1,26 +1,8 @@
 @extends('index')
 
 @section('content')
-<style>
-    .top-content{
-        background-color: #d9d9d9;
-    }
-</style>
-<div class="container">
-    <h4 class="text-center mb-4" style="text-decoration:underline">EXAM TIME TABLE </h4>
-    {{-- <h6 class="text-center mb-4">Academic_Year {{ session('academic_year') }} </h6> --}}
-    <table border=1 style="border-collapse:collapse;" class="top-content table table-bordered">
-        <tbody>
-            <tr>
-                <td><strong>Standard: </strong>{{ session('std') }}</td>
-                <td><strong>Division: </strong>{{ session('dv') }}</td>
-                <td><strong>Branch ID: </strong>{{ session('branch_id') }}</td>
-                <td><strong>Academic Year: </strong>{{ session('academic_year') }}</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
 <div class="container mt-3">
+    <h4 class="text-center mb-4" style="text-decoration:underline">EXAM TIME TABLE </h4>
     <form id="attendForm" method="post" action="{{ route('examTimeTable') }}">
         @csrf <!-- This will generate the CSRF token field -->
         <div class="mb-3">
