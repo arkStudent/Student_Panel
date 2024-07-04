bea5b05ffd8205c413c20184667d2249877d8df5
gazala1628 <cm23jm1304@gmail.com>
2024-07-04 14:37:57 +0530

    gazala: handled error in fees balance

--- app/Http/Controllers/FeesController.php
+++ app/Http/Controllers/FeesController.php
@@ -117,6 +117,29 @@ class FeesController extends Controller
         // dd($cat_id);
 
         // Constructing the SQL query
+        if(!$std){
+            $query = "SELECT
+            b.head,
+            CASE WHEN b.head_id = 16 THEN (b.amount * 12) ELSE b.amount END AS total_amount,
+            a.amount AS paid_amount
+        FROM
+            sal_fees_bhead b
+        LEFT JOIN (
+            SELECT
+                bhead_id,
+                SUM(amount) AS amount
+            FROM
+                sal_fees_trans
+            WHERE
+                student_id = '$stud_id'
+            GROUP BY
+                bhead_id
+        ) a ON b.head_id = a.bhead_id
+        WHERE
+            b.branch_id = '$branch_id'
+            AND b.academic_year = '$academic_year'";
+        $feeBal = DB::select($query);
+        }else{
         $query = "SELECT
             b.head,
             CASE WHEN b.head_id = 16 THEN (b.amount * 12) ELSE b.amount END AS total_amount,
@@ -139,8 +162,8 @@ class FeesController extends Controller
             AND b.std = '$std'
             AND b.academic_year = '$academic_year'
             AND b.category_id IN (" . implode(',', $cat_id) . ")";
-
         $feeBal = DB::select($query);
+    }
 
         return view('fees.balanceFees', compact('academic_year', 'feeBal', 'category'));
     }
