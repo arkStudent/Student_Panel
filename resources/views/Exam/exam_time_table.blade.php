a5e0dc50e40ac9637df1a53ab2af4f152ba0fcdf
gazala1628 <cm23jm1304@gmail.com>
2024-07-04 14:52:12 +0530

    gazala: records sorted by date

--- resources/views/Exam/exam_time_table.blade.php
+++ resources/views/Exam/exam_time_table.blade.php
@@ -11,6 +11,9 @@ use Carbon\Carbon;
     .subjects{
         font-size: 15px;
     }
+    .top-content{
+        background-color: #d9d9d9;
+    }
 </style>
 
 <div class="container mt-3">
@@ -18,9 +21,20 @@ use Carbon\Carbon;
     <div class="card">
         <div class="card-header text-center">
             <h4 class="card-title">Exam Time Table <br>
-                <span class="text-muted" style="font-size:17px;">Academic year {{ session('academic_year') }}</span></h4>
+                <span class="text-muted" style="font-size:17px;">Academic year {{ session('academic_year') }}</span></h4>        
+        <div class="container">
+            <table border=1 style="border-collapse:collapse;" class="top-content table table-bordered">
+                <tbody>
+                    <tr>
+                        <td><strong>Standard: </strong>{{ session('std') }}</td>
+                        <td><strong>Division: </strong>{{ session('dv') }}</td>
+                        <td><strong>Branch Name: </strong>{{ isset($branch_name->sname) ? $branch_name->sname : '-' }}</td>
+                        <td><strong>Academic Year: </strong>{{ session('academic_year') }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-
+    </div>
         <!-- /.card-header -->
         <div class="card-body">
                 <div>
@@ -28,10 +42,10 @@ use Carbon\Carbon;
                         <thead class="text-center thead-dark">
                             <tr>
                                 <th rowspan="2">Sl.no</th>
+                                <th rowspan="2">Subject</th>
                                 <th rowspan="2">Date</th>
                                 <th rowspan="2">Day</th>
-                                <th colspan="2">Time</th>
-                                <th rowspan="2">Subject</th>
+                                <th colspan="2">Time</th>   
                                 <th rowspan="2">I Marks</th>
                                 <th rowspan="2">W Marks</th>
                             </tr>
@@ -45,12 +59,11 @@ use Carbon\Carbon;
                             @forelse ($examTimeDetails as $timeDetails)
                             <tr>
                                 <td>{{ $loop->iteration }}</td>
+                                <td>{{ $timeDetails->subject }}</td>
                                 <td>{{ Carbon::parse($timeDetails->date)->format('d-m-Y') }}</td>
-                                {{-- <td>{{ $timeDetails->date }}</td> --}}
                                 <td>{{ $timeDetails->day }}</td>
                                 <td>{{ $timeDetails->from_time }}</td>
                                 <td>{{ $timeDetails->to_time }}</td>
-                                <td>{{ $timeDetails->subject }}</td>
                                 <td>{{ $timeDetails->i_marks }}</td>
                                 <td>{{ $timeDetails->w_marks }}</td>
                             </tr>                               
