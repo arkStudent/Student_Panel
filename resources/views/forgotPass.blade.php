97ca6aa7c356b21804eba6984bb04fcaa9f57593
gazala1628 <cm23jm1304@gmail.com>
2024-07-04 14:55:43 +0530

    gazala: placed forgot password out of template

--- resources/views/forgotPass.blade.php
+++ resources/views/forgotPass.blade.php
@@ -1,56 +1,113 @@
-@extends('index')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-    <div class="container m-5">
+<head>
+    <title>Forgot Password</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="">
+    <meta name="author" content="Phoenixcoded" />
+
+    <!-- CSRF Token Meta Tag -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- vendor css -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <form id="forgotPass" method="post">
+        @csrf
+        <div class="auth-wrapper">
+            <div class="auth-content text-center">
+                <div class="card borderless">
+                    <div class="row align-items-center ">
+                        <div class="col-md-12">
+                            <div class="card-body">
+                                <h4 class="mb-3 f-w-400">Forgot Password</h4>
+                                <hr>
+                                <div class="form-group mb-3">
+                                    <input type="text" class="form-control" id="s_id" name="s_id"
+                                        placeholder="Enter Student Id" required>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="text" class="form-control" id="pass" name="pass"
+                                        placeholder="Enter New Password" required>
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input type="password" class="form-control" id="cpass" name="cpass"
+                                        placeholder="Confirm New Password" required>
+                                </div>
+                                <button type="submit" class="btn btn-block btn-primary mb-4">Submit</button>
+                                <hr>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    {{-- <div class="container m-5">
         <h5 class="text-center">FORGOT PASSWORD</h5>
         <form id="forgotPass">
             @csrf
             <div class="mb-3">
                 <label for="password" class="form-label">New Password</label>
-                <input type="text" class="form-control" id="pass" name="pass" placeholder="Enter New Password"
-                    required>
+                <input type="text" class="form-control" id="pass" name="pass"
+                    placeholder="Enter New Password" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Confirm Password</label>
-                <input type="text" class="form-control" id="cpass" name="cpass" placeholder="Confirm New Password"
-                    required>
+                <input type="text" class="form-control" id="cpass" name="cpass"
+                    placeholder="Confirm New Password" required>
             </div>
             <div class="mb-3">
                 <center><button type="submit" class="btn btn-primary mt-3">Submit</button></center>
             </div>
         </form>
-    </div>
+    </div> --}}
 
-    <script>
-        document.getElementById('forgotPass').addEventListener('submit', function(e) {
-            e.preventDefault();
-            var form = e.target;
-            var formData = new FormData(form);
-            var csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+    <!-- Required Js -->
+    <script src="{{ asset('js/vendor-all.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/pcoded.min.js') }}"></script>
 
-            fetch('{{ route('forgotPass') }}', {
-                    method: 'POST',
-                    headers: {
-                        'X-CSRF-TOKEN': csrfToken
-                    },
-                    body: formData
-                })
-                .then(response => {
-                    if (!response.ok) {
-                        return response.json().then(errorData => {
-                            throw errorData
-                        });
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    alert('Password set successfully');
-                })
-                .catch((error) => {
-                    const msg = Object.values(error).flat().join('\n');
-                    alert(msg);
-                });
+</body>
+
+<script>
+    document.getElementById('forgotPass').addEventListener('submit', function(e) {
+        e.preventDefault();
+        var form = e.target;
+        var formData = new FormData(form);
+        var csrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+        fetch('{{ route('forgotPass') }}', {
+                method: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken
+                },
+                body: formData
+            })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        throw errorData
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                alert('Password set successfully');
+                window.location.href = '{{ route('login') }}';
+            })
+            .catch((error) => {
+                const msg = Object.values(error).flat().join('\n');
+                alert(msg);
                 location.reload();
-        });
-    </script>
-@endsection
+            });
+    });
+</script>
