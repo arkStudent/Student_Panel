84ae38ca35e1262ca6e06b94bbd8618af75909ab
sanakousarjahagirdar <sanakousarjahagirdar@gmail.com>
2024-07-06 10:26:55 +0530

    sana:added the header

--- resources/views/Academic/eventheader.blade.php
+++ resources/views/Academic/eventheader.blade.php
@@ -2,18 +2,20 @@
     .top-content{
         background-color: #d9d9d9;
     }
+    
+    .table th,
+    .table td {
+        white-space: normal;  
+        max-width: 400px;  
+    }
+ 
 </style>
-<<<<<<< HEAD
 <div class="container">
     <table border=1 style="border-collapse:collapse;" class="top-content table table-bordered">
         <tbody>
-            
-                {{-- <td><strong>Branch ID: </strong>{{ $branch->id }}</td> 
-                <td><strong>Branch Name: </strong>{{ session('branch_id') }}</td>
-                  <td><strong>H.M: </strong>{{ session('hm') }}</td>
-                <td><strong>Class Teacher: </strong>{{ session('class_teacher') }}</td> --}}
+             
                 <tr> 
-                    <td><strong>Branch Name: </strong>{{ $branch->sname }}</td> 
+                    <td><strong>Branch Name: </strong>{{ $branch->name }}</td> 
                     <td><strong>H.M: </strong>{{ $hm }}</td>
                     <td><strong>Class Teacher: </strong>{{ $class_teacher }}</td>
                 </tr>
@@ -23,16 +25,3 @@
 </div>
 
  
-=======
-<div class="container mt-3">
-    <table border=1 style="border-collapse:collapse;" class="top-content table table-bordered">
-        <tbody>
-            <tr>
-                <td><strong>Branch Name: </strong>{{ session('branch_name') }}</td>
-                <td><strong>H.M: </strong>{{ session('hm') }}</td>
-                <td><strong>Class Teacher: </strong>{{ session('class_teacher') }}</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
->>>>>>> origin/master
