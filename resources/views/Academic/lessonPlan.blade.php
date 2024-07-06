de688f666750183c03cedd14933bbb767ade5555
sanakousarjahagirdar <sanakousarjahagirdar@gmail.com>
2024-07-06 10:28:53 +0530

    sana:removing the unwanted code

--- resources/views/Academic/lessonPlan.blade.php
+++ resources/views/Academic/lessonPlan.blade.php
@@ -3,22 +3,13 @@
 @section('content')
     <div class="container">
         <div class="text-center mb-4">
-<<<<<<< HEAD
             <h4 style="margin-bottom: 10px; text-decoration: underline;">Lesson Plan</h4> <!-- Adding margin-bottom to create space -->
                    </div>
-=======
-            <h3>Lesson Plan</h3>
-        </div>
->>>>>>> origin/master
         <form id="subjectForm" method="POST">
             @csrf
             <div class="mb-3">
                 <label for="subject" class="form-label">Select Subject:</label>
-<<<<<<< HEAD
                 <select id="subject" name="subject" class="form-control" required>
-=======
-                <select id="subject" name="subject" class="form-control">
->>>>>>> origin/master
                     <option value="" selected disabled>Select Subject</option>
                     @foreach ($subjects as $subject)
                         <option value="{{ $subject->sub_id }}">{{ $subject->sname }}</option>
@@ -27,23 +18,17 @@
             </div>
             <div class="text-center">
                 <button type="submit" class="btn btn-primary">Submit</button>
-<<<<<<< HEAD
                 <button type="button" class="btn btn-danger" onclick="resetForm()">Reset</button>
           
-=======
->>>>>>> origin/master
             </div>
         </form>
     </div>
 
     <script>
-<<<<<<< HEAD
          function resetForm() {
             document.getElementById("subjectForm").reset();
         }
          
-=======
->>>>>>> origin/master
         document.getElementById('subjectForm').addEventListener('submit', function(event) {
             event.preventDefault();
 
