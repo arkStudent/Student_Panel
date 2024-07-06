28dbefdbebf2ceaae7f460e3ae0bf95486ef4eb8
sanakousarjahagirdar <sanakousarjahagirdar@gmail.com>
2024-07-06 10:33:39 +0530

    sana:create the new lesson plan header file

---
+++ resources/views/Academic/lessonPlanheader.blade.php
@@ -0,0 +1,25 @@
+<style>
+    .card {
+        margin-top: 20px;
+        box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    }
+
+    .card-header {
+        background-color: #f0f0f0;
+        padding: 10px;
+    }
+
+    .card-body {
+        padding: 20px;
+    }
+
+    .top-content {
+        background-color: #d9d9d9;
+    }
+    .table th,
+    .table td {
+        white-space: normal;  
+        max-width: 400px;  
+    }
+    
+</style>
